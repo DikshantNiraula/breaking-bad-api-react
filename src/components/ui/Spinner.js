91ee44fe23a8a43f25a7d2afc2c39333919efc8e
Dikshant <codeonlinedikshant@gmail.com>
2020-07-20 12:18:54 +0545

    initial commit

---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import spin from "../../img/spinner1.gif";
+const Spinner = () => {
+  return (
+    <div>
+      <img src={spin} style={{ margin: "auto", display: "flex" }} />
+    </div>
+  );
+};
+
+export default Spinner;
